a15a0d1f6cdb872ebb2227f082b9b0aa155e986f
Kyle Lortscher <kylelortscher@gmail.com>
2016-04-20 11:04:06 -0700

    Added bcrypt and user model

--- models/user.js
+++ models/user.js
@@ -39,3 +39,5 @@ userSchema.pre('save', function(next){
 userSchema.methods.comparePassword = function(password) {
     return bcrypt.compareSync(password, this.password);
 };
+
+module.exports = mongoose.model('User', userSchema);
